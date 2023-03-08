421386206db27805466460cbc60b765861fbf499
vijeshbalakrishnan89 <vijeshbalakrishnan89@gmail.com>
2023-03-09 00:29:30 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel</title>
+	<title>Sample Webpage</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Welcome to laravel routing</h1>
+	<header>
+		<h1>Sample Webpage</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Us</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section >
+			<h1 id="api-response">Section 1</h1>
+		</section>
+		<section>
+			<h2>Section 1</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</section>
+		<section>
+			<h2>Section 2</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</section>
+		<section>
+			<h2>Section 3</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</section>
+	</main>
+	<footer>
+		<p>&copy; 2023 Sample Webpage</p>
+	</footer>
 </body>
 </html>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script>
+	$(document).ready(function () {
+		$.ajax({
+			url: 'http://127.0.0.1:8000/api/hello/world',
+			type: "GET",
+			dataType: "json",
+			success: function (data) {
+				document.getElementById("api-response").innerHTML = data.message; 
+			}
+		});
+	});
+</script>
